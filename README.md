99ede29d46b6d456b322c44bcc5e2c682ba257b8
SeppPenner <HansM2013@gmx.de>
2020-12-27 21:11:43 +0100

    Removed license year, moved changelog to extra file, adjusted folder structure, updated nuget packages, moved to Net 5.0.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ MoveFilesToTopFolder
 ====================================
 
 MoveFilesToTopFolder is an example project to move all mp3 files from a directory to one top folder.
-The executable was written and tested in .Net 4.8.
+The executable was written and tested in .Net 5.0.
 
 [![Build status](https://ci.appveyor.com/api/projects/status/dwd868ofr04o4b58?svg=true)](https://ci.appveyor.com/project/SeppPenner/movefilestotopfolder)
 [![GitHub issues](https://img.shields.io/github/issues/SeppPenner/MoveFilesToTopFolder.svg)](https://github.com/SeppPenner/MoveFilesToTopFolder/issues)
@@ -11,9 +11,7 @@ The executable was written and tested in .Net 4.8.
 [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://raw.githubusercontent.com/SeppPenner/MoveFilesToTopFolder/master/License.txt)
 [![Known Vulnerabilities](https://snyk.io/test/github/SeppPenner/MoveFilesToTopFolder/badge.svg)](https://snyk.io/test/github/SeppPenner/MoveFilesToTopFolder)
 
-
 Change history
 --------------
 
-* **Version 1.0.1.0 (2019-10-27)** : Updated nuget packages, added GitVersionTask.
-* **Version 1.0.0.0 (2019-05-05)** : 1.0 release.
+See the [Changelog](https://github.com/SeppPenner/MoveFilesToTopFolder/blob/master/Changelog.md).
---
+++ src/MoveFilesToTopFolder/Program.cs
@@ -0,0 +1,96 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Hämmer Electronics">
+//   Copyright (c) All rights reserved.
+// </copyright>
+// <summary>
+//   The main program.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MoveFilesToTopFolder
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// The main program.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        public static void Main()
+        {
+            TrySearchDirectory(@"E:\\Others", @"E:\\Musik");
+            Console.Write("Finished, exit with enter");
+            Console.Read();
+        }
+
+        /// <summary>
+        /// Tries to search the directory.
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory.</param>
+        /// <param name="targetDirectory">The target directory.</param>
+        private static void TrySearchDirectory(string sourceDirectory, string targetDirectory)
+        {
+            try
+            {
+                SearchFilesAndDirectories(sourceDirectory, targetDirectory);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + ex.StackTrace);
+            }
+        }
+
+        /// <summary>
+        /// Searches files and directories.
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory.</param>
+        /// <param name="targetDirectory">The target directory.</param>
+        private static void SearchFilesAndDirectories(string sourceDirectory, string targetDirectory)
+        {
+            CopyFiles(sourceDirectory, targetDirectory);
+            SearchDirectories(sourceDirectory, targetDirectory);
+        }
+
+        /// <summary>
+        /// Copies the files.
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory.</param>
+        /// <param name="targetDirectory">The target directory.</param>
+        private static void CopyFiles(string sourceDirectory, string targetDirectory)
+        {
+            foreach (var file in Directory.GetFiles(sourceDirectory, "*.mp3"))
+            {
+                Console.WriteLine("Copying file from " + file + " to " + Path.Combine(targetDirectory, Path.GetFileName(file)));
+                var targetFile = Path.Combine(targetDirectory, Path.GetFileName(file));
+
+                if (!File.Exists(targetFile))
+                {
+                    File.Move(file, Path.Combine(targetDirectory, Path.GetFileName(file)));
+                }
+                else
+                {
+                    var fileParts = Path.GetFileName(file).Split('-');
+                    targetFile = fileParts[0] + " (2)-" + fileParts[1];
+                    File.Move(file, Path.Combine(targetDirectory, targetFile));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Searches the directories.
+        /// </summary>
+        /// <param name="sourceDirectory">The source directory.</param>
+        /// <param name="targetDirectory">The target directory.</param>
+        private static void SearchDirectories(string sourceDirectory, string targetDirectory)
+        {
+            foreach (var dir in Directory.GetDirectories(sourceDirectory))
+            {
+                TrySearchDirectory(dir, targetDirectory);
+            }
+        }
+    }
+}
