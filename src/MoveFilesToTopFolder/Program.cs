120c82cb0476518eb190d6d330346df2035ea694
SeppPenner <HansM2013@gmx.de>
2022-02-15 16:31:22 +0100

    Added nullable checks, added editorconfig, added file scoped namespaces, added global usings.

--- src/MoveFilesToTopFolder/Program.cs
+++ src/MoveFilesToTopFolder/Program.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file="Program.cs" company="Hämmer Electronics">
 //   Copyright (c) All rights reserved.
 // </copyright>
@@ -7,90 +7,86 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace MoveFilesToTopFolder
+namespace MoveFilesToTopFolder;
+
+/// <summary>
+/// The main program.
+/// </summary>
+public class Program
 {
-    using System;
-    using System.IO;
+    /// <summary>
+    /// The main method.
+    /// </summary>
+    public static void Main()
+    {
+        TrySearchDirectory(@"E:\\Others", @"E:\\Musik");
+        Console.Write("Finished, exit with enter");
+        Console.Read();
+    }
 
     /// <summary>
-    /// The main program.
+    /// Tries to search the directory.
     /// </summary>
-    public class Program
+    /// <param name="sourceDirectory">The source directory.</param>
+    /// <param name="targetDirectory">The target directory.</param>
+    private static void TrySearchDirectory(string sourceDirectory, string targetDirectory)
     {
-        /// <summary>
-        /// The main method.
-        /// </summary>
-        public static void Main()
+        try
         {
-            TrySearchDirectory(@"E:\\Others", @"E:\\Musik");
-            Console.Write("Finished, exit with enter");
-            Console.Read();
+            SearchFilesAndDirectories(sourceDirectory, targetDirectory);
         }
-
-        /// <summary>
-        /// Tries to search the directory.
-        /// </summary>
-        /// <param name="sourceDirectory">The source directory.</param>
-        /// <param name="targetDirectory">The target directory.</param>
-        private static void TrySearchDirectory(string sourceDirectory, string targetDirectory)
+        catch (Exception ex)
         {
-            try
+            Console.WriteLine(ex.Message + ex.StackTrace);
+        }
+    }
+
+    /// <summary>
+    /// Searches files and directories.
+    /// </summary>
+    /// <param name="sourceDirectory">The source directory.</param>
+    /// <param name="targetDirectory">The target directory.</param>
+    private static void SearchFilesAndDirectories(string sourceDirectory, string targetDirectory)
+    {
+        CopyFiles(sourceDirectory, targetDirectory);
+        SearchDirectories(sourceDirectory, targetDirectory);
+    }
+
+    /// <summary>
+    /// Copies the files.
+    /// </summary>
+    /// <param name="sourceDirectory">The source directory.</param>
+    /// <param name="targetDirectory">The target directory.</param>
+    private static void CopyFiles(string sourceDirectory, string targetDirectory)
+    {
+        foreach (var file in Directory.GetFiles(sourceDirectory, "*.mp3"))
+        {
+            Console.WriteLine("Copying file from " + file + " to " + Path.Combine(targetDirectory, Path.GetFileName(file)));
+            var targetFile = Path.Combine(targetDirectory, Path.GetFileName(file));
+
+            if (!File.Exists(targetFile))
             {
-                SearchFilesAndDirectories(sourceDirectory, targetDirectory);
+                File.Move(file, Path.Combine(targetDirectory, Path.GetFileName(file)));
             }
-            catch (Exception ex)
+            else
             {
-                Console.WriteLine(ex.Message + ex.StackTrace);
-            }
-        }
-
-        /// <summary>
-        /// Searches files and directories.
-        /// </summary>
-        /// <param name="sourceDirectory">The source directory.</param>
-        /// <param name="targetDirectory">The target directory.</param>
-        private static void SearchFilesAndDirectories(string sourceDirectory, string targetDirectory)
-        {
-            CopyFiles(sourceDirectory, targetDirectory);
-            SearchDirectories(sourceDirectory, targetDirectory);
-        }
-
-        /// <summary>
-        /// Copies the files.
-        /// </summary>
-        /// <param name="sourceDirectory">The source directory.</param>
-        /// <param name="targetDirectory">The target directory.</param>
-        private static void CopyFiles(string sourceDirectory, string targetDirectory)
-        {
-            foreach (var file in Directory.GetFiles(sourceDirectory, "*.mp3"))
-            {
-                Console.WriteLine("Copying file from " + file + " to " + Path.Combine(targetDirectory, Path.GetFileName(file)));
-                var targetFile = Path.Combine(targetDirectory, Path.GetFileName(file));
-
-                if (!File.Exists(targetFile))
-                {
-                    File.Move(file, Path.Combine(targetDirectory, Path.GetFileName(file)));
-                }
-                else
-                {
-                    var fileParts = Path.GetFileName(file).Split('-');
-                    targetFile = fileParts[0] + " (2)-" + fileParts[1];
-                    File.Move(file, Path.Combine(targetDirectory, targetFile));
-                }
+                var fileParts = Path.GetFileName(file).Split('-');
+                targetFile = fileParts[0] + " (2)-" + fileParts[1];
+                File.Move(file, Path.Combine(targetDirectory, targetFile));
             }
         }
+    }
 
-        /// <summary>
-        /// Searches the directories.
-        /// </summary>
-        /// <param name="sourceDirectory">The source directory.</param>
-        /// <param name="targetDirectory">The target directory.</param>
-        private static void SearchDirectories(string sourceDirectory, string targetDirectory)
+    /// <summary>
+    /// Searches the directories.
+    /// </summary>
+    /// <param name="sourceDirectory">The source directory.</param>
+    /// <param name="targetDirectory">The target directory.</param>
+    private static void SearchDirectories(string sourceDirectory, string targetDirectory)
+    {
+        foreach (var dir in Directory.GetDirectories(sourceDirectory))
         {
-            foreach (var dir in Directory.GetDirectories(sourceDirectory))
-            {
-                TrySearchDirectory(dir, targetDirectory);
-            }
+            TrySearchDirectory(dir, targetDirectory);
         }
     }
 }
